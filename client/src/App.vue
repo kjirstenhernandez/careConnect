bdce5e83f3ae70acfc239808f6733cad38ee2719
Kjirsten Hernandez <sandlot7665@gmail.com>
2025-07-23 23:50:00 -0600

    Added footer

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,10 +1,9 @@
 <script setup lang="ts">
 import NavBar from './components/NavBar.vue';
+import Bottom from './components/Footer.vue';
 </script>
 <template>
   <NavBar />
-  <div class="bg-blue-500 text-white p-4">
-    Tailwind should make this blue with white text
-  </div>
+  <Bottom />
 </template>
 <style></style>

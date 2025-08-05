6960f52525b76976d1661aad334cce236455f417
Kjirsten Hernandez <sandlot7665@gmail.com>
2025-08-05 15:19:42 -0600

    Added route for getting clinic info by ID

--- server/routes/clinics.ts
+++ server/routes/clinics.ts
@@ -1,3 +1,7 @@
 import express, { Router } from 'express';
+import { getClinicInfoByID } from '../controllers/clinics';
+import { validateObjectId } from '../middleware/validateObjectId';
 
 export const clinics: Router = express.Router();
+
+clinics.get('/find/:clinicId', validateObjectId, getClinicInfoByID);

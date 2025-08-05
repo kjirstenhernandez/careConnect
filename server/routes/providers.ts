da9fbc2ee5f54741e422ed06b1043665f1e439e9
Kjirsten Hernandez <sandlot7665@gmail.com>
2025-08-05 14:30:35 -0600

    added new route to add location to provider

--- server/routes/providers.ts
+++ server/routes/providers.ts
@@ -1,8 +1,13 @@
 import express, { Router } from 'express';
-import { addProvider, getProviderById } from '../controllers/providers';
+import {
+  addProvider,
+  addProviderLocation,
+  getProviderById,
+} from '../controllers/providers';
 import { validateObjectId } from '../middleware/validateObjectId';
 
 export const providers: Router = express.Router();
 
 providers.get('/:providerId', validateObjectId, getProviderById);
-providers.post('/', addProvider);
+providers.post('/new', addProvider);
+providers.post('/addlocation', addProviderLocation);

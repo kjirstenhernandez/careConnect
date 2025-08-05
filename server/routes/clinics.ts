b73e9d149a32a359c56dd919a7d73aa9e7a64f71
Kjirsten Hernandez <sandlot7665@gmail.com>
2025-08-05 16:40:55 -0600

    Added post route to create new clinic

--- server/routes/clinics.ts
+++ server/routes/clinics.ts
@@ -1,7 +1,8 @@
 import express, { Router } from 'express';
-import { getClinicInfoByID } from '../controllers/clinics';
+import { addClinic, getClinicInfoByID } from '../controllers/clinics';
 import { validateObjectId } from '../middleware/validateObjectId';
 
 export const clinics: Router = express.Router();
 
 clinics.get('/find/:clinicId', validateObjectId, getClinicInfoByID);
+clinics.post('/new', addClinic);

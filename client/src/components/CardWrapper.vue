687c3544ae59cd745b7a06a14de437fb5d051542
Kjirsten Hernandez <sandlot7665@gmail.com>
2025-08-05 17:21:30 -0600

    Created bones of a cardwrapper to hold the provider/clinic cards

---
+++ client/src/components/CardWrapper.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container">
+    <p
+      class="error"
+      v-if="error"
+    >
+      {{ error }}
+    </p>
+    <div class="provider-card-container">
+      <div class="card"><!-- add 'v-for=' when ready to populate cards --></div>
+    </div>
+  </div>
+</template>
+<script></script>

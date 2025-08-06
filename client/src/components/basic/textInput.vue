19cc818e10cb47a56bfdae617d0dde96d20125b5
Kjirsten Hernandez <sandlot7665@gmail.com>
2025-08-06 13:35:44 -0600

    Adjusted placeholder to work with props

--- client/src/components/basic/textInput.vue
+++ client/src/components/basic/textInput.vue
@@ -2,7 +2,6 @@
   <input
     :placeholder="placeholder"
     :class="[
-      'w-full',
       'px-4 py-2 text-gray-800 placeholder-gray-400 bg-white border border-gray-300 rounded-xl shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition duration-150',
       textClass,
     ]"

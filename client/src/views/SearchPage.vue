cf86d9b0d160615b67cc20bd86194c2b53ebdb19
Kjirsten Hernandez <sandlot7665@gmail.com>
2025-08-06 12:24:55 -0600

    Renamed file

---
+++ client/src/views/SearchPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div
+      class="flex flex-col sm:flex-row items-center gap-4 w-full max-w-xl mt-4"
+    >
+      <textInput
+        placeholder="Clinic or Provider Name"
+        textClass="w-full sm:w-[300px]"
+      />
+      <textInput
+        placeholder="Location"
+        textClass="w-full sm:w-[150px]"
+      />
+      <ButtonTemplate type="submit">Search</ButtonTemplate>
+    </div>
+  </div>
+</template>
+<script setup lang="ts"></script>

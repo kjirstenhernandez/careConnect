77baf11b12ddf9398907ecd10b2c3636d157f2d8
Kjirsten Hernandez <sandlot7665@gmail.com>
2025-08-08 10:57:58 -0600

    Updated search to watch for query changes

--- client/src/fuse/fuseSearch.ts
+++ client/src/fuse/fuseSearch.ts
@@ -1,28 +1,38 @@
-import { ref, computed, watch } from 'vue';
+import { ref, watch } from 'vue';
 import Fuse from 'fuse.js';
 import type { IFuseOptions } from 'fuse.js';
 
 export function useFuseSearch<T>(items: T[], keys: string[]) {
   const query = ref('');
   const results = ref<{ item: T }[]>([]);
-  let fuse: Fuse<T> | null = null;
-
   const options: IFuseOptions<T> = {
     keys,
     threshold: 0.3,
-    includeScore: true,
+    includeScore: true, // we want the scores to be able to sort by relevance later
   };
 
-  fuse = new Fuse(items, options);
+  let fuse = new Fuse(items, options);
 
   watch(
     query,
     (newQuery) => {
       results.value = newQuery
-        ? fuse!.search(newQuery).map((result) => ({ item: result.item }))
+        ? fuse.search(newQuery).map((r) => ({ item: r.item }))
         : items.map((item) => ({ item }));
     },
     { immediate: true }
   );
+
+  watch(
+    () => items,
+    (newItems) => {
+      fuse = new Fuse(newItems, options);
+      results.value = query.value
+        ? fuse.search(query.value).map((r) => ({ item: r.item }))
+        : newItems.map((item) => ({ item }));
+    },
+    { immediate: true, deep: true }
+  );
+
   return { query, results };
 }

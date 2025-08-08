0a2fd86f916f05d92e1ad0fbd258cb9d28097cd8
Kjirsten Hernandez <sandlot7665@gmail.com>
2025-08-08 14:32:53 -0600

    changed fuseSearch to a service

--- client/src/services/fuseSearchService.ts
+++ client/src/services/fuseSearchService.ts
@@ -1,30 +1,33 @@
-import { ref, watch } from 'vue';
+import { ref, watch, type Ref } from 'vue';
 import Fuse from 'fuse.js';
 import type { IFuseOptions } from 'fuse.js';
 
-export function useFuseSearch<T>(items: T[], keys: string[]) {
-  const query = ref('');
+export function useFuseSearch<T>(
+  items: Ref<T[]>,
+  keys: string[],
+  query: Ref<string>
+) {
   const results = ref<{ item: T }[]>([]);
   const options: IFuseOptions<T> = {
     keys,
     threshold: 0.3,
-    includeScore: true, // set to true so we can use the scores for relevance sorting later
+    includeScore: true,
   };
 
-  let fuse = new Fuse(items, options);
+  let fuse = new Fuse(items.value, options);
 
   watch(
     query,
     (newQuery) => {
       results.value = newQuery
         ? fuse.search(newQuery).map((r) => ({ item: r.item }))
-        : items.map((item) => ({ item }));
+        : items.value.map((item) => ({ item }));
     },
     { immediate: true }
   );
 
   watch(
-    () => items,
+    () => items.value,
     (newItems) => {
       fuse = new Fuse(newItems, options);
       results.value = query.value
@@ -34,5 +37,5 @@ export function useFuseSearch<T>(items: T[], keys: string[]) {
     { immediate: true, deep: true }
   );
 
-  return { query, results };
+  return { results };
 }

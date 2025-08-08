3669c7877365515ba6060d47f413de280bac9db7
Kjirsten Hernandez <sandlot7665@gmail.com>
2025-08-08 14:33:27 -0600

    Moved the search bar into it's own component

---
+++ client/src/components/SearchBar.vue
@@ -0,0 +1,28 @@
+<template>
+  <div
+    class="flex flex-col sm:flex-row items-center gap-4 w-full max-w-3xl mt-4"
+  >
+    <textInput
+      :placeholder="`Find ${type}`"
+      textClass="w-full sm:w-2/3"
+      v-model="query"
+    />
+    <textInput
+      placeholder="Location"
+      textClass="w-full sm:w-1/3"
+      v-model="location"
+    />
+    <ButtonTemplate
+      type="submit"
+      @click="emit('search')"
+      >Search</ButtonTemplate
+    >
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps, defineEmits } from 'vue';
+
+defineProps<{ type: string; query: string; location: string }>();
+const emit = defineEmits(['search']);
+</script>

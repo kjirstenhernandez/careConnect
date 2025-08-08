824dabb3a2aae44be5c82320b21ef88e48cf6f24
Kjirsten Hernandez <sandlot7665@gmail.com>
2025-08-08 10:26:11 -0600

    Moved the search bar component out of the Hero

--- client/src/components/Hero.vue
+++ client/src/components/Hero.vue
@@ -11,23 +11,7 @@
     <h2 class="text-2xl leading-8 text-gray-600 text-center">
       Find contact information for individual providers or entire clinics.
     </h2>
-    <!-- <div
-      class="flex flex-col sm:flex-row items-center gap-4 w-full max-w-xl mt-4"
-    >
-      <textInput
-        placeholder="Clinic or Provider Name"
-        textClass="w-full sm:w-[300px]"
-      />
-      <textInput
-        placeholder="Location"
-        textClass="w-full sm:w-[150px]"
-      />
-      <ButtonTemplate type="submit">Search</ButtonTemplate>
-    </div> -->
   </div>
 </template>
 
-<script setup lang="ts">
-// import textInput from '@/components/basic/textInput.vue';
-// import ButtonTemplate from '../components/basic/buttonTemplate.vue';
-</script>
+<script setup lang="ts"></script>

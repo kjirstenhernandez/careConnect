2fe6864569e3aac49c0c289a753f5071fa995ee9
Kjirsten Hernandez <sandlot7665@gmail.com>
2025-08-09 16:30:22 -0600

    Fixed binding and props

--- client/src/components/CardWrapper.vue
+++ client/src/components/CardWrapper.vue
@@ -9,9 +9,9 @@
     <div class="card-container">
       <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         <component
-          v-for="item in items"
-          :key="item.id"
-          v-bind="item"
+          v-for="itemObj in items"
+          :key="itemObj.item.id"
+          v-bind="itemObj.item"
           :is="cardType"
         ></component>
       </div>
@@ -22,7 +22,7 @@
 <script setup lang="ts">
 import ProviderCard from '@/components/ProviderCard.vue';
 import ClinicCard from '@/components/ClinicCard.vue';
-import { ref } from 'vue';
+import { ref, watch } from 'vue';
 const error = ref<string | null>(null);
 
 const props = defineProps<{
@@ -36,9 +36,15 @@ let cardMap = {
 } as const;
 
 let cardType;
-if (props.type === 'provider') {
+if (props.type === 'providers') {
   cardType = cardMap.providers;
 } else {
   cardType = cardMap.clinics;
 }
+watch(
+  () => props.items,
+  () => {
+    console.log('CardWrapper Prop.items: ' + { ...props.items });
+  }
+);
 </script>

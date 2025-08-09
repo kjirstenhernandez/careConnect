1787ff70757d31da8a3312d0c5059b8860902daf
Kjirsten Hernandez <sandlot7665@gmail.com>
2025-08-08 23:21:46 -0600

    Added interpolation and logic

--- client/src/components/CardWrapper.vue
+++ client/src/components/CardWrapper.vue
@@ -6,9 +6,32 @@
     >
       {{ error }}
     </p>
-    <div class="provider-card-container">
-      <div class="card"><!-- add 'v-for=' when ready to populate cards --></div>
+    <div class="card-container">
+      <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        <component
+          v-for="item in items"
+          :key="item.id"
+          v-bind="item"
+          :is="cardMap[type]"
+        ></component>
+      </div>
     </div>
   </div>
 </template>
-<script></script>
+
+<script setup lang="ts">
+import ProviderCard from '@/components/ProviderCard.vue';
+import ClinicCard from '@/components/ClinicCard.vue';
+import { ref } from 'vue';
+const error = ref<string | null>(null);
+
+const props = defineProps<{
+  type: 'providers' | 'clinics';
+  items: any[];
+}>();
+
+const cardMap = {
+  providers: ProviderCard,
+  clinics: ClinicCard,
+} as const;
+</script>

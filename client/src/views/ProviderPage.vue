40970f4c0da1b7ba050b433d205129f1e26d3606
Kjirsten Hernandez <sandlot7665@gmail.com>
2025-08-08 19:07:57 -0600

    Added CSS

---
+++ client/src/views/ProviderPage.vue
@@ -0,0 +1,8 @@
+<template>
+  <section class="relative bg-accent-[#AEDFE5] bg-opacity-05 py-16">
+    <div
+      class="absolute left-0 top-0 h-full w-full bg-hero-pattern-doctor bg-cover bg-right-top bg-no-repeat md:bg-contain"
+    ></div>
+  </section>
+</template>
+<script setup lang="ts"></script>

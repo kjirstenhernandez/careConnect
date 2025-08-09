64f248b0cf63032cfd2ef21955d788220eba8fa6
Kjirsten Hernandez <sandlot7665@gmail.com>
2025-08-09 01:56:28 -0600

    Adjusted routes

--- server/routes/clinics.ts
+++ server/routes/clinics.ts
@@ -1,9 +1,13 @@
 import express, { Router } from 'express';
-import { addClinic, getClinicInfoByID } from '../controllers/clinics';
+import {
+  addClinic,
+  getAllClinics,
+  getClinicInfoByID,
+} from '../controllers/clinics';
 import { validateObjectId } from '../middleware/validateObjectId';
 
 export const clinics: Router = express.Router();
 
-clinics.get('/find/clinics/:clinicId', validateObjectId, getClinicInfoByID);
+clinics.get('/find/:clinicId', validateObjectId, getClinicInfoByID);
 clinics.post('/new', addClinic);
-clinics.get('/find/clinics');
+clinics.get('/find', getAllClinics);
--- server/routes/providers.ts
+++ server/routes/providers.ts
@@ -2,6 +2,7 @@ import express, { Router } from 'express';
 import {
   addProvider,
   addProviderLocation,
+  getAllProviders,
   getProviderById,
 } from '../controllers/providers';
 import { validateObjectId } from '../middleware/validateObjectId';
@@ -9,5 +10,6 @@ import { validateObjectId } from '../middleware/validateObjectId';
 export const providers: Router = express.Router();
 
 providers.get('/find/:providerId', validateObjectId, getProviderById);
+providers.get('/find', getAllProviders);
 providers.post('/new', addProvider);
 providers.post('/addlocation', addProviderLocation);

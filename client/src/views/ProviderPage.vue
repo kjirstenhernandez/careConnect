f818824113667799d22d69717bbaef4176a2c5d0
Kjirsten Hernandez <sandlot7665@gmail.com>
2025-08-09 01:58:52 -0600

    Added component population statements for testing

--- client/src/views/ProviderPage.vue
+++ client/src/views/ProviderPage.vue
@@ -2,7 +2,14 @@
   <section class="relative bg-accent-[#AEDFE5] bg-opacity-05 py-16">
     <div
       class="absolute left-0 top-0 h-full w-full bg-hero-pattern-doctor bg-cover bg-right-top bg-no-repeat md:bg-contain"
-    ></div>
+    >
+      <h1>Hello {{ providerId }}</h1>
+    </div>
   </section>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useRoute } from 'vue-router';
+const route = useRoute();
+
+const providerId = route.params.id;
+</script>

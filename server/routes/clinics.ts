a1e42d79acf43708f70b2d9386c4ae0af98fb593
Kjirsten Hernandez <sandlot7665@gmail.com>
2025-08-10 02:08:32 -0600

    Added new route

--- server/routes/clinics.ts
+++ server/routes/clinics.ts
@@ -3,11 +3,13 @@ import {
   addClinic,
   getAllClinics,
   getClinicInfoByID,
+  getMultipleClinicsByIDs,
 } from '../controllers/clinics';
 import { validateObjectId } from '../middleware/validateObjectId';
 
 export const clinics: Router = express.Router();
 
 clinics.get('/find/:clinicId', validateObjectId, getClinicInfoByID);
+clinics.post('/find/many', getMultipleClinicsByIDs);
 clinics.post('/new', addClinic);
 clinics.get('/find', getAllClinics);

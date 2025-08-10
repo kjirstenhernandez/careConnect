d8e40d0cd027e0de9c59a791b491481f2f29bed0
Kjirsten Hernandez <sandlot7665@gmail.com>
2025-08-09 19:36:34 -0600

    Fixed logic to get query from SearchBar and reactively update page

--- client/src/views/SearchPage.vue
+++ client/src/views/SearchPage.vue
@@ -1,11 +1,15 @@
 <template>
   <div class="relative py-12 flex flex-col items-center lg:py-20">
     <div class="container flex flex-col items-center">
-      <SearchTabs @change="handleNavClick" />
+      <SearchTabs
+        :type="type"
+        @change="handleNavClick"
+      />
       <SearchBar
         :type="type"
         v-model:query="queryValue"
         v-model:location="location"
+        @update:query="queryValue = $event"
         @search="handleSearchClick"
       />
     </div>
@@ -35,6 +39,7 @@ import { useFilteredResults } from '@/services/FilteredResultsService';
 import { fetchSearchData } from '@/services/ApiSearchService';
 import type { Provider, Clinic } from '../interfaces';
 import CardWrapper from '@/components/CardWrapper.vue';
+import { computed } from 'vue';
 
 type SearchItem = Provider | Clinic;
 
@@ -44,14 +49,37 @@ const type = ref(route.params.type as string);
 const keyword = ref('');
 const queryValue = ref('');
 const location = ref('');
-const data = ref<SearchItem[]>([]);
+const data = ref<SearchItem[]>([]); //raw API data
 
-const { results: fuseResults } = useFuseSearch(data, [], keyword);
+const keysMap = {
+  providers: ['firstName', 'lastName', 'specialty'],
+  clinics: ['name', 'city'],
+  specialties: ['specialty', 'firstName', 'lastName'],
+};
+
+const keys = computed(
+  () => keysMap[type.value as 'providers' | 'clinics' | 'specialties'] || []
+);
+
+const { results: fuseResults } = useFuseSearch(data, keys, keyword);
+
 const filteredResults = useFilteredResults(type, location, fuseResults);
 
 async function loadData() {
-  data.value = await fetchSearchData(type.value);
+  const response = (await fetchSearchData(type.value)) as {
+    clinicInfo?: Clinic[];
+    providers?: Provider[];
+    message?: string;
+  };
+  if (type.value === 'clinics') {
+    data.value = response.clinicInfo ?? [];
+  } else if (type.value === 'providers') {
+    data.value = response.providers ?? [];
+  }
 }
+watch(filteredResults, (newVal) => {
+  console.log('filteredResults changed:', newVal);
+});
 
 function handleSearchClick() {
   keyword.value = queryValue.value.trim();
@@ -62,6 +90,11 @@ function handleNavClick(newType: string) {
   router.push({ name: 'SearchPage', params: { type: newType } });
   loadData();
 }
-/* Optional: We could add a watch to have it populate as the user types, but skip the initial load 
-by adding an if (isInitialLoad){...} statement. */
+
+watch(
+  () => route.params.type,
+  (newType) => {
+    type.value = newType as string;
+  }
+);
 </script>

e3a23da54cd0b92b36a3add509a3e9d483d7fc10
Kjirsten Hernandez <sandlot7665@gmail.com>
2025-08-10 16:35:31 -0600

    minor log change

--- server/index.ts
+++ server/index.ts
@@ -22,5 +22,5 @@ app.get(/^\/(?!api).*/, (_, res) => {
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });

0308d45aa052fac34f2e59d96ff2bdbeeb41e408
kjirstenhernandez <122486287+kjirstenhernandez@users.noreply.github.com>
2025-08-11 03:31:36 -0600

    Added documentaiton comments

--- server/routes/clinics.ts
+++ server/routes/clinics.ts
@@ -7,9 +7,28 @@ import {
 } from '../controllers/clinics';
 import { validateObjectId } from '../middleware/validateObjectId';
 
+
+/**
+ * Router for clinic-related endpoints.
+ *
+ * Provides API routes to:
+ * - Retrieve clinic info by ID
+ * - Retrieve multiple clinics by IDs
+ * - Add a new clinic
+ * - Retrieve all clinics
+ */
 export const clinics: Router = express.Router();
 
+// GET -- information for a single clinic by ID
+// uses validateObjectId middleware to verify the validity of the route param 'clinicId'
 clinics.get('/find/:clinicId', validateObjectId, getClinicInfoByID);
-clinics.post('/find/many', getMultipleClinicsByIDs);
-clinics.post('/new', addClinic);
+
+//GET -- information for all clinics in the database
 clinics.get('/find', getAllClinics);
+
+//POST -- retrieve multiple clinic Objects matching the IDs given in teh req.body
+clinics.post('/find/many', getMultipleClinicsByIDs);
+
+//POST -- adds a new clinic to the database using the info in the req.body
+clinics.post('/new', addClinic);
+

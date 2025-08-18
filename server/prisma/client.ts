c08c2f02eb304944c4fa195aa982027005528b7b
Kjirsten Hernandez <sandlot7665@gmail.com>
2025-08-18 12:29:04 -0600

    Removed server folder

--- server/prisma/client.ts
+++
@@ -1,6 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-
-// creates and exports one instance of the prisma client that can be reused in other areas
-const prisma = new PrismaClient();
-
-export default prisma;
--- server/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { providers } from './providers';
-import { clinics } from './clinics';
-const router = express.Router();
-
-router.use('/providers', providers);
-router.use('/clinics', clinics);
-
-export default router;

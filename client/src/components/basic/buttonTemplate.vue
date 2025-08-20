379160d9ecb53b7da354d1b462a3bc4e4354ab65
Kjirsten Hernandez <sandlot7665@gmail.com>
2025-08-20 02:00:05 -0600

    Added props for CSS

--- client/src/components/basic/buttonTemplate.vue
+++ client/src/components/basic/buttonTemplate.vue
@@ -2,7 +2,12 @@
   <button
     :type="type"
     :disabled="disabled"
-    class="inline-flex items-center justify-center px-6 py-2 rounded-xl font-semibold text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 disabled:opacity-50 disabled:cursor-not-allowed transition duration-150"
+    :class="[
+      'inline-flex items-center justify-center px-6 py-2 rounded-xl font-semibold   focus:outline-none focus:ring-2 focus:ring-blue-500 disabled:opacity-50 disabled:cursor-not-allowed transition duration-150',
+      textColorHex,
+      backgroundColor,
+      backgroundHover,
+    ]"
     @click="$emit('click')"
   >
     <slot />
@@ -13,6 +18,9 @@
 defineProps<{
   type?: 'button' | 'submit' | 'reset';
   disabled?: boolean;
+  textColorHex: string;
+  backgroundColor: string;
+  backgroundHover: string;
 }>();
 
 defineEmits(['click']);

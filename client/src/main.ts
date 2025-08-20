024ecebda07e07252ec80cf4b9f14cd6b3918c77
Kjirsten Hernandez <sandlot7665@gmail.com>
2025-08-20 02:00:29 -0600

    Started adding Auth0

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,3 +1,4 @@
+import { createAuth0 } from '@auth0/auth0-vue';
 import { createApp } from 'vue';
 import App from './App.vue';
 import './assets/main.css';
@@ -6,6 +7,14 @@ import router from './router';
 
 const app = createApp(App);
 
-app.use(router);
+app.use(router).use(
+  createAuth0({
+    domain: import.meta.env.VITE_AUTH0_DOMAIN,
+    clientId: import.meta.env.VITE_AUTH0_CLIENT_ID,
+    authorizationParams: {
+      redirect_url: import.meta.env.VITE_AUTH0_CALLBACK_URL,
+    },
+  })
+);
 
 app.mount('#app');
